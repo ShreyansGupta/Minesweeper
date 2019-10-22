7f1c72ba1e02ee0af31f3e8524bf86247f309a8d
Chaitanya <nalamchaitanya@gmail.com>
2019-10-22 08:48:03 -0400

    First reveal of cell shouldn't be bomb

--- Environment.py
+++ Environment.py
@@ -16,32 +16,16 @@ class Environment:
         self.mazeCells = np.reshape(self.mazeCells,(d,d))
 
 
-    def reveal(self,row,column):
-        if self.mazeCells[row][column] == -1 :
-            return -1;  
-        
+    def reveal(self,row,column, first = False):
+        if first == False and self.mazeCells[row][column] == -1 :
+            return -1;
         count = 0
-        if row >= 1:
-            if column>=1 and self.mazeCells[row-1][column-1] == -1:
-                count +=1
-            if column<self.dimension-1 and self.mazeCells[row-1][column+1] == -1:
-                count +=1
-            if self.mazeCells[row-1][column] == -1:
-                count +=1
-
-        if column >= 1:
-            if row<self.dimension-1 and self.mazeCells[row+1][column-1] == -1:
-                count +=1
-            if self.mazeCells[row][column-1] == -1:
-                count +=1
-
-        if row < self.dimension-1:
-            if column<self.dimension-1 and self.mazeCells[row+1][column+1] == -1:
-                count +=1
-            if self.mazeCells[row+1][column] == -1:
-                count +=1
-
-        if column < self.dimension-1 and self.mazeCells[row][column+1] == -1:
-            count +=1
-        
+        for i in [-1,0,1]:
+            for j in [-1,0,1]:
+                if(i == 0 and j == 0):
+                    continue;
+                elif 0<=row+i<self.dimension and 0<=column+j<self.dimension :
+                    if self.mazeCells[row+i][column+j] == -1:
+                        count +=1
+        self.mazeCells[row][column] == count;
         return count
